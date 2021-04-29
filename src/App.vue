571914ecb9371ce86bbcc7c2469c9630b26d82f6
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-04-29 11:17:29 -0700

    :sparkles: Add home page view

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app></v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Vuedio</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <v-col class="text-center" cols="12">
+        Designed by Wolfgang Wazzle Strauss
+      </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+  data: () => ({
+    drawer: null,
+  }),
+});
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="mt-12">
+    <ControlBar></ControlBar>
+    <v-container class="mx-4">
+      <NoteList name="Kick"></NoteList>
+      <NoteList name="Snare"></NoteList>
+      <NoteList name="Hi-Hat"></NoteList>
+    </v-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import ControlBar from "@/components/ControlBar.vue";
+import NoteList from "@/components/NoteList.vue";
+
+export default Vue.extend({
+  name: "Home",
+  components: {
+    ControlBar,
+    NoteList,
+  },
+});
+</script>
