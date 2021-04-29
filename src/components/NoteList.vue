721df53d5bdf590993967359e1bd5f681d7507bc
Macklan Weinstein <wolfgang.wazzle.strauss@gmail.com>
2021-04-29 11:18:19 -0700

    :sparkles: Add NoteList component

---
+++ src/components/NoteList.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row no-gutters>
+    <v-col cols="2">
+      <v-card
+        class="font-weight-bold mr-4 pl-4 py-1 secondary lighten-4 text-uppercase"
+      >
+        {{ name }}
+      </v-card>
+    </v-col>
+    <v-col :key="note.index" v-for="note of notes">
+      <v-card
+        class="mr-1 primary"
+        height="32"
+        hover
+        :class="{ 'lighten-4': !note.active }"
+        @click="note.active = !note.active"
+      ></v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NoteList",
+  data: () => ({
+    notes: Array.from({ length: 16 }, (_, index) => ({ active: false, index })),
+  }),
+  props: ["name"],
+});
+</script>
