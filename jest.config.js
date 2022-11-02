e157bda4f77798f3ffa425bd9feb8c9a22fa056e
Macklan Weinstein <macklan.weinstein@gmail.com>
2022-11-01 22:32:29 -0700

    :wrench: Switch to Vite for project tooling

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// Jest configuration file for testing JavaScript and TypeScript code.
-//
-// For more information, visit https://jestjs.io/docs/configuration.
-
-module.exports = {
-  collectCoverage: false,
-  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
-  testMatch: ["**/tests/**/*.spec.ts"],
-};
